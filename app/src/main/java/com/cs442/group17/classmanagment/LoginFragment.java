49e9ce130531eef17e3e31df749c36a6d73fcde5
Kibbles <dbornsten@gmail.com>
2016-11-23 22:52:18 -0600

    Final merge of all work. Consider this the baseline to work from

--- app/src/main/java/com/cs442/group17/classmanagment/LoginFragment.java
+++ app/src/main/java/com/cs442/group17/classmanagment/LoginFragment.java
@@ -1,17 +1,28 @@
 package com.cs442.group17.classmanagment;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by USER on 23-11-16.
@@ -25,8 +36,11 @@ public class LoginFragment extends Fragment {
     public static final String userIdPref = "userIdPref";
     public static final String userEmailPref = "userEmailPref";
     public static final String userRoleIdPref = "userRoleIdPref";
+    public static final String userCollIdPref = "userCollIdPref";
     SharedPreferences sharedpreferences;
     int roleId;
+    int collId;
+    String collegeName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,20 +52,52 @@ public class LoginFragment extends Fragment {
         editTextPass = (EditText) view.findViewById(R.id.editTextPass);
         dbHandler = new MyDBHandler(getActivity(), null, null, 1);//move thisto main activity, pass in to fragment
 
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                collegeName = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+
+        });
+
+        // Spinner Drop down elements
+        List<String> colleges;
+        colleges = dbHandler.getColleges();
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, colleges);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
         Button buttonLogin = (Button) view.findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                collId = dbHandler.getCollegeIdByCollegeName(collegeName);
                 if(editTextEmail.getText().toString().isEmpty() || editTextPass.getText().toString().isEmpty())
                 {
-                    Toast.makeText(getActivity(), "Email and Password can not be blank.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Email and Password can not be blank", Toast.LENGTH_SHORT).show();
                 }
-                else if(dbHandler.checkLogin(editTextEmail.getText().toString(), editTextPass.getText().toString()))
+                else if(dbHandler.checkLogin(editTextEmail.getText().toString(), editTextPass.getText().toString(), collId))
                 {
                     String em = editTextEmail.getText().toString();
                     int userId = dbHandler.getUserIdByEmail(em);
                     String name = dbHandler.getNameById(userId);
                     roleId = dbHandler.getRoleByUserId(userId);
+
+                    checkNotifications(userId);
 
                     SharedPreferences prefs = getActivity().getSharedPreferences(MyPREFERENCES, getActivity().MODE_PRIVATE);
                     String userNamePrefs = prefs.getString(userNamePref, null);
@@ -61,6 +107,7 @@ public class LoginFragment extends Fragment {
                         editor.putInt(userIdPref, userId);
                         editor.putString(userEmailPref, em);
                         editor.putInt(userRoleIdPref, roleId);
+                        editor.putInt(userCollIdPref, collId);
                         editor.commit();
 
                         Toast.makeText(getActivity(), "Hi, " + name, Toast.LENGTH_SHORT).show();
@@ -70,7 +117,7 @@ public class LoginFragment extends Fragment {
                 }
                 else
                 {
-                    Toast.makeText(getActivity(), "Invalid Login.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Invalid login", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -93,6 +140,32 @@ public class LoginFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void checkNotifications(int userId) {
+        ArrayList<Notifications> notiCollection = dbHandler.getNotificatiosFromDB(userId);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getActivity());
+
+        //Building Notification
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("Ticker.....");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("You have " + notiCollection.size() + " new approvals");
+
+        //notification.setContentText("Body");
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        notification.setSound(alarmSound);
+        long[] vibrate = { 0, 100, 200, 300 };
+        notification.setVibrate(vibrate);
+
+        Intent intent = new Intent(getActivity(), ClassSelStuActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Issuing Notification
+        NotificationManager nm = (NotificationManager) getActivity().getSystemService(getActivity().NOTIFICATION_SERVICE);
+        nm.notify(5526, notification.build());
     }
 
     private void clearPreferences() {
